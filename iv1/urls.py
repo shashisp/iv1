56da14d5c2be8b8c9470c99ef61d5ed0de95f167
Shashi Kumar P <shashiessp@gmail.com>
2013-07-04 12:48:24 +0530

    added template dir

--- iv1/urls.py
+++ iv1/urls.py
@@ -1,17 +1,17 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic.simple import direct_to_template
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'iv1.views.home', name='home'),
+    url(r'^$', 'direct_to_template', {'template':'index.html'}),
     # url(r'^iv1/', include('iv1.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
